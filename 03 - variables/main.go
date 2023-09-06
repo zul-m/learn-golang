672e3554dec871f425077609eda9a996723feeb7
Zulfikar Muhamad <zul.m@icloud.com>
2023-09-06 16:35:04 +0800

    Assigned value to the variable

--- 03 - variables/main.go
+++ 03 - variables/main.go
@@ -5,4 +5,8 @@ import "fmt"
 func main() {
 	var age int // variable declaration
 	fmt.Println("My age is", age)
+	age = 29 // assignment
+	fmt.Println("My age is", age)
+	age = 54 // assignment
+	fmt.Println("My age is", age)
 }
