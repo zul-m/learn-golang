75b734303c0aec44c3cf06576f5108ae5ea7bab6
Zulfikar Muhamad <zul.m@icloud.com>
2023-09-07 12:22:40 +0800

    Removed type

--- 03 - variables/main.go
+++ 03 - variables/main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	var width, height int = 100, 50 // declaring multiple variables
+	var width, height = 100, 50 // "int" is dropped
 
 	fmt.Println("Width is", width, "and height is", height)
 }
