670469a882fde728b2b8b5d98634aee92f55db43
Zulfikar Muhamad <zul.m@icloud.com>
2023-09-07 12:25:22 +0800

    Unspecified initial value

--- 03 - variables/main.go
+++ 03 - variables/main.go
@@ -3,7 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var width, height = 100, 50 // "int" is dropped
-
+	var width, height int
 	fmt.Println("Width is", width, "and height is", height)
+	width = 100
+	height = 50
+	fmt.Println("New width is", width, "and new height is", height)
 }
