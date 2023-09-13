8cfb1f08062f05a7f78edd592bfc6f03eacb92f1
Zulfikar Muhamad <zul.m@icloud.com>
2023-09-13 18:11:34 +0800

    Newly declared variables

--- 03 - variables/main.go
+++ 03 - variables/main.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
-	count := 10
-	fmt.Println("Count =", count)
+	a, b := 20, 30 // declare variables a and b
+	fmt.Println("a is", a, "and b is", b)
+	b, c := 40, 50 // b is already declared but c is new
+	fmt.Println("b is", b, "and c is", c)
+	b, c = 80, 90 // assign new values to the already declared variables b and c
+	fmt.Println("New b is", b, "and c is", c)
 }
