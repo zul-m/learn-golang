f97612a1c084a31ed891701a5042e3c67c5aaa2c
Zulfikar Muhamad <zul.m@icloud.com>
2023-09-13 18:14:26 +0800

    Computed variable

--- 03 - variables/main.go
+++ 03 - variables/main.go
@@ -1,12 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	a, b := 20, 30 // declare variables a and b
-	fmt.Println("a is", a, "and b is", b)
-	b, c := 40, 50 // b is already declared but c is new
-	fmt.Println("b is", b, "and c is", c)
-	b, c = 80, 90 // assign new values to the already declared variables b and c
-	fmt.Println("New b is", b, "and c is", c)
+	a, b := 145.8, 543.8
+	c := math.Min(a, b)
+	fmt.Println("Minimum value is", c)
 }
